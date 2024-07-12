f4f28c1b08ec29407ea4d67a110ac29317760661
Soumyakanti Pan <soumyakanti8051@gmail.com>
2024-07-12 16:56:52 -0700

    update mysolve

--- R/mysolve.R
+++ R/mysolve.R
@@ -1,10 +1,19 @@
-#' Solves the linear system Ax = b where A is positive definite
+#' Solve a positive-definite linear system
+#'
+#' Solves the linear system \eqn{Ax = b}{Ax = b} where \eqn{A}{A} is a positive-definite matrix and \eqn{b}{b} is a vector.
 #'
 #' @useDynLib spStackdev
-#' @param A an \eqn{n\times n}{nxn} positive definite matrix
+#' @param A an \eqn{n\times n}{nxn} positive-definite matrix
 #' @param b an \eqn{n\times 1}{nx1} matrix/vector
 #' @returns The \eqn{n\times 1}{nx1} solution to the system
 #' @export
+#' @examples
+#' A <- rbind(c(1, 2, 3),
+#'            c(2, 5, 7),
+#'            c(3, 7, 14))
+#' b <- c(1, 0, 1)
+#' (x <- mysolve(A, b)) # 5 -2 0
+#'
 #' @keywords utilities
 mysolve <- function(A, b){
 
