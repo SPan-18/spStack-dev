42d26cba3db1e67ae77d34b953a22c02868d321a
Soumyakanti Pan <soumyakanti8051@gmail.com>
2024-07-21 04:35:34 -0700

    Update spLMexact.cpp
    sample spatial effects

--- src/spLMexact.cpp
+++ src/spLMexact.cpp
@@ -23,7 +23,7 @@ extern "C" {
      Common variables
      *****************************************/
     // int i, j, k, info, nProtect = 0;
-    int i, j, info, nProtect = 0;
+    int i, j, s, info, nProtect = 0;
     char const *lower = "L";
     char const *nUnit = "N";
     char const *ntran = "N";
@@ -31,7 +31,7 @@ extern "C" {
     // char const *rside = "R";
     char const *lside = "L";
     const double one = 1.0;
-    // const double negOne = -1.0;
+    const double negOne = -1.0;
     const double zero = 0.0;
     const int incOne = 1;
 
@@ -117,14 +117,15 @@ extern "C" {
      *****************************************/
     double sigmaSqIGaPost = 0, sigmaSqIGbPost = 0;
     double sse = 0;
+    double dtemp = 0, dtemp2 = 0;
 
     double *Vz = (double *) R_alloc(nn, sizeof(double)); zeros(Vz, nn);
-    double *Vy = (double *) R_alloc(nn, sizeof(double)); zeros(Vy, nn);
     double *thetasp = (double *) R_alloc(2, sizeof(double));
 
     double *tmp_n = (double *) R_alloc(n, sizeof(double)); zeros(tmp_n, n);
     double *tmp_np = (double *) R_alloc(np, sizeof(double)); zeros(tmp_np, np);
     double *tmp_nn = (double *) R_alloc(nn, sizeof(double)); zeros(tmp_nn, nn);
+    double *tmp_nn2 = (double *) R_alloc(nn, sizeof(double)); zeros(tmp_nn2, nn);
 
     double *tmp_p1 = (double *) R_alloc(p, sizeof(double)); zeros(tmp_p1, p);
     double *tmp_p2 = (double *) R_alloc(p, sizeof(double)); zeros(tmp_p2, p);
@@ -134,20 +135,20 @@ extern "C" {
     //construct covariance matrix
     thetasp[0] = phi;
     thetasp[1] = nu;
-    spCorLT(coordsD, n, thetasp, corfn, Vz);
-    F77_NAME(dcopy)(&nn, Vz, &incOne, Vy, &incOne);
+    spCorFull(coordsD, n, thetasp, corfn, Vz);
+    F77_NAME(dcopy)(&nn, Vz, &incOne, tmp_nn, &incOne);
     for(i = 0; i < n; i++){
-      Vy[i*n + i] += deltasq;
+      tmp_nn[i*n + i] += deltasq;
     }
 
     // find sse
-    F77_NAME(dcopy)(&nn, Vy, &incOne, tmp_nn, &incOne);
     F77_NAME(dpotrf)(lower, &n, tmp_nn, &n, &info FCONE); if(info != 0){error("c++ error: dpotrf failed\n");}
 
     F77_NAME(dcopy)(&n, Y, &incOne, tmp_n, &incOne);
     F77_NAME(dtrsv)(lower, ntran, nUnit, &n, tmp_nn, &n, tmp_n, &incOne FCONE FCONE FCONE);
 
-    sse += pow(F77_NAME(dnrm2)(&n, tmp_n, &incOne), 2);
+    dtemp = pow(F77_NAME(dnrm2)(&n, tmp_n, &incOne), 2);
+    sse += dtemp;
 
     F77_NAME(dcopy)(&np, X, &incOne, tmp_np, &incOne);
     F77_NAME(dtrsm)(lside, lower, ntran, nUnit, &n, &p, &one, tmp_nn, &n, tmp_np, &n FCONE FCONE FCONE FCONE);
@@ -158,8 +159,8 @@ extern "C" {
     F77_NAME(dpotri)(lower, &p, tmp_pp, &p, &info FCONE); if(info != 0){error("c++ error: dpotri failed\n");}
     F77_NAME(dsymv)(lower, &p, &one, tmp_pp, &p, betaMu, &incOne, &zero, tmp_p2, &incOne FCONE);
 
-    double ssetemp = F77_CALL(ddot)(&p, betaMu, &incOne, tmp_p2, &incOne);
-    sse += ssetemp;
+    dtemp = F77_CALL(ddot)(&p, betaMu, &incOne, tmp_p2, &incOne);
+    sse += dtemp;
 
     F77_NAME(daxpy)(&p, &one, tmp_p2, &incOne, tmp_p1, &incOne);
     F77_NAME(dgemm)(ytran, ntran, &p, &p, &n, &one, tmp_np, &n, tmp_np, &n, &zero, tmp_pp2, &p FCONE FCONE);
@@ -169,7 +170,17 @@ extern "C" {
     F77_NAME(dpotrf)(lower, &p, tmp_pp, &p, &info FCONE); if(info != 0){error("c++ error: dpotrf failed\n");}
     F77_NAME(dtrsv)(lower, ntran, nUnit, &p, tmp_pp, &p, tmp_p2, &incOne FCONE FCONE FCONE);
 
-    sse -= pow(F77_NAME(dnrm2)(&p, tmp_p2, &incOne), 2);
+    dtemp = pow(F77_NAME(dnrm2)(&p, tmp_p2, &incOne), 2);
+    sse -= dtemp;
+
+    // set-up for sampling spatial random effects
+    F77_NAME(dcopy)(&nn, Vz, &incOne, tmp_nn2, &incOne);
+    F77_NAME(dtrsm)(lside, lower, ntran, nUnit, &n, &n, &one, tmp_nn, &n, tmp_nn2, &n FCONE FCONE FCONE FCONE);
+    F77_NAME(dgemm)(ytran, ntran, &n, &n, &n, &one, tmp_nn2, &n, tmp_nn2, &n, &zero, tmp_nn, &n FCONE FCONE);
+    F77_NAME(dcopy)(&nn, Vz, &incOne, tmp_nn2, &incOne);
+    F77_NAME(daxpy)(&nn, &negOne, tmp_nn, &incOne, tmp_nn2, &incOne);
+    F77_NAME(dpotrf)(lower, &n, tmp_nn2, &n, &info FCONE); if(info != 0){error("c++ error: dpotrf failed\n");}
+    mkLT(tmp_nn2, n);
 
     // posterior parameters of sigmaSq
     sigmaSqIGaPost += sigmaSqIGa;
@@ -181,32 +192,57 @@ extern "C" {
     // posterior samples of sigma-sq and beta
     SEXP samples_sigmaSq_r = PROTECT(allocVector(REALSXP, nSamples)); nProtect++;
     SEXP samples_beta_r = PROTECT(allocMatrix(REALSXP, p, nSamples)); nProtect++;
+    SEXP samples_z_r = PROTECT(allocMatrix(REALSXP, n, nSamples)); nProtect++;
 
     double sigmaSq = 0;
     double *beta = (double *) R_alloc(p, sizeof(double)); zeros(beta, p);
+    double *z = (double *) R_alloc(n, sizeof(double)); zeros(z, n);
 
     GetRNGstate();
 
-    for(i = 0; i < nSamples; i++){
-      sigmaSq = 1.0 / rgamma(sigmaSqIGaPost, 1.0 / sigmaSqIGbPost);
-      REAL(samples_sigmaSq_r)[i] = sigmaSq;
+    for(s = 0; s < nSamples; s++){
+      // sample sigmaSq from its marginal posterior
+      dtemp = 1.0 / sigmaSqIGbPost;
+      dtemp = rgamma(sigmaSqIGaPost, dtemp);
+      sigmaSq = 1.0 / dtemp;
+      REAL(samples_sigmaSq_r)[s] = sigmaSq;
 
+      // sample fixed effects by composition sampling
+      dtemp = sqrt(sigmaSq);
       for(j = 0; j < p; j++){
-        beta[j] = rnorm(0.0, sqrt(sigmaSq));
+        beta[j] = rnorm(0.0, dtemp);
       }
       F77_NAME(daxpy)(&p, &one, tmp_p2, &incOne, beta, &incOne);
       F77_NAME(dtrsv)(lower, ytran, nUnit, &p, tmp_pp, &p, beta, &incOne FCONE FCONE FCONE);
 
       for(j = 0; j < p; j++){
-        REAL(samples_beta_r)[i*p + j] = beta[j];
+        REAL(samples_beta_r)[s*p + j] = beta[j];
       }
+
+      // sample spatial effects by composition sampling
+      F77_NAME(dcopy)(&n, Y, &incOne, tmp_n, &incOne);
+      F77_NAME(dgemv)(ntran, &n, &p, &negOne, X, &n, beta, &incOne, &one, tmp_n, &incOne FCONE);
+      dtemp2 = 1.0 / deltasq;
+      F77_NAME(dscal)(&n, &dtemp2, tmp_n, &incOne);
+
+      for(i = 0; i < n; i++){
+        z[i] = rnorm(0.0, dtemp);
+      }
+
+      F77_NAME(dgemv)(ytran, &n, &n, &one, tmp_nn2, &n, tmp_n, &incOne, &one, z, &incOne FCONE);
+      F77_NAME(dgemv)(ntran, &n, &n, &one, tmp_nn2, &n, z, &incOne, &zero, tmp_n, &incOne FCONE);
+
+      for(i = 0; i < n; i++){
+        REAL(samples_z_r)[s*n + i] = tmp_n[i];
+      }
+
     }
 
     PutRNGstate();
 
     // make return object for posterior samples of sigma-sq and beta
     SEXP result_r, resultName_r;
-    int nResultListObjs = 2;
+    int nResultListObjs = 3;
 
     result_r = PROTECT(allocVector(VECSXP, nResultListObjs)); nProtect++;
     resultName_r = PROTECT(allocVector(VECSXP, nResultListObjs)); nProtect++;
@@ -219,20 +255,24 @@ extern "C" {
     SET_VECTOR_ELT(result_r, 1, samples_sigmaSq_r);
     SET_VECTOR_ELT(resultName_r, 1, mkChar("sigmaSq"));
 
+    // samples of z
+    SET_VECTOR_ELT(result_r, 2, samples_z_r);
+    SET_VECTOR_ELT(resultName_r, 2, mkChar("z"));
+
     namesgets(result_r, resultName_r);
 
     // SEXP samples_beta_r = PROTECT(allocMatrix(REALSXP, nSamples, p)); nProtect++;
-    // SEXP beta_mean_r = PROTECT(allocVector(REALSXP, p)); nProtect++;
-    // SEXP betaV_r = PROTECT(allocMatrix(REALSXP, p, p)); nProtect++;
+    // SEXP tmp_n_r = PROTECT(allocVector(REALSXP, n)); nProtect++;
+    // SEXP tmp_nn_r = PROTECT(allocMatrix(REALSXP, n, n)); nProtect++;
 
-    // for (i = 0; i < p; i++) {
-    //   for (j = 0; j < p; j++) {
-    //     REAL(betaV_r)[i*p + j] = tmp_pp[i*p + j];
+    // for (i = 0; i < n; i++) {
+    //   for (j = 0; j < n; j++) {
+    //     REAL(tmp_nn_r)[i*n + j] = tmp_nn2[i*n + j];
     //   }
     // }
 
-    // for(i = 0; i < p; i++){
-    //   REAL(beta_mean_r)[i] = tmp_p2[i];
+    // for(i = 0; i < n; i++){
+    //   REAL(tmp_n_r)[i] = tmp_n[i];
     // }
 
     // REAL(result_r)[0] = sigmaSqIGaPost;
