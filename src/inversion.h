8b4d8ac35673f6438cb5177df493933ced5de8d9
Soumyakanti Pan <soumyakanti8051@gmail.com>
2024-07-27 21:17:13 -0700

    test hot function

--- src/inversion.h
+++ src/inversion.h
@@ -6,3 +6,9 @@ void inversionLM(double *X, int n, int p, double deltasq, double *VbetaInv,
                  double *tmp_n1, double *tmp_n2, double *tmp_p1,
                  double *tmp_pp, double *tmp_np1, double*tmp_np2,
                  double *outp, double *outn);
+
+void inversionLM2(double *X, int n, int p, double deltasq, double *VbetaInv,
+                  double *Vz, double *cholVy, double *v1, double *v2,
+                  double *out_p, double *out_n);
+
+void zeros2(double *x, int length);
