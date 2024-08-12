0b07c20018969dd199dbd5a9f9221a5fafbcfce8
Soumyakanti Pan <soumyakanti8051@gmail.com>
2024-08-12 01:44:23 -0700

    add candidate_models fn

--- R/utils.R
+++ R/utils.R
@@ -30,7 +30,33 @@ parseFormula <- function(formula, data, intercept = TRUE, justX = FALSE) {
 
 }
 
-
+# internal function: checks if input is integer
 is_integer <- function(x) {
     is.numeric(x) && (floor(x) == x)
+}
+
+#' Create candidate models
+#'
+#' @description `candidate_models` creates an `stack.model.list` object
+#' that is passed into the functions `spLMstack` or `spGLMstack`.
+#' @details Supply a list with each entry containing candidate values of the
+#' parameter of the same name as that entry. See example below.
+#' @param params_list a list containing candidate values of parameters
+#' required for the model that you want to fit. For names of the parameters,
+#' look up the function that implements the model. See example below.
+#' @return a `stack.model.list` object
+#' @examples
+#' mod_list <- list(phi = c(3, 5, 10),
+#'                  nu = c(0.5, 1, 1.5))
+#' cand_mods <- candidate_models(mod_list)
+#' # pass this to spLMstack() in the mod_params argument
+#' @export
+candidate_models <- function(params_list){
+
+    models <- expand.grid(params_list)
+    models_list <- apply(models, 1, function(x) as.vector(x, mode = "list"))
+
+    class(models_list) <- "spStack.model.list"
+
+    return(models_list)
 }
