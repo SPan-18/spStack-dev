8ea5e1b0921ade84eeddc6d2d0f990df8e79cba9
Soumyakanti Pan <soumyakanti8051@gmail.com>
2024-09-28 19:01:14 -0700

    correct F to FALSE

--- R/cholUpdate.R
+++ R/cholUpdate.R
@@ -48,14 +48,14 @@
 #' v <- 1:n
 #' APlusvvT <- A + tcrossprod(v)
 #' cholA1 <- t(chol(APlusvvT))
-#' cholA2 <- cholUpdateRankOne(cholA, v, lower = F)
+#' cholA2 <- cholUpdateRankOne(cholA, v, lower = FALSE)
 #' print(all(abs(cholA1 - cholA2) < 1E-9))
 #'
 #' ## Single Row-deletion update
 #' ind <- 2
 #' A1 <- A[-ind, -ind]
 #' cholA1 <- t(chol(A1))
-#' cholA2 <- cholUpdateDel(cholA, del.index = ind, lower = F)
+#' cholA2 <- cholUpdateDel(cholA, del.index = ind, lower = FALSE)
 #' print(all(abs(cholA1 - cholA2) < 1E-9))
 #'
 #' ## Block-deletion update
@@ -64,7 +64,7 @@
 #' del_ind <- c(start_ind:end_ind)
 #' A1 <- A[-del_ind, -del_ind]
 #' cholA1 <- t(chol(A1))
-#' cholA2 <- cholUpdateDelBlock(cholA, start_ind, end_ind, lower = F)
+#' cholA2 <- cholUpdateDelBlock(cholA, start_ind, end_ind, lower = FALSE)
 #' print(all(abs(cholA1 - cholA2) < 1E-9))
 NULL
 
